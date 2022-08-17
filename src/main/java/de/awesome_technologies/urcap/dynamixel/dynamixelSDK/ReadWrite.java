de4291ea8067ce29b1928b16c04f10c6baae80b9
FNitzsche <>
2022-08-17 16:03:50 +0200

    Add classes for dynamixel communication

---
+++ src/main/java/de/awesome_technologies/urcap/dynamixel/dynamixelSDK/ReadWrite.java
@@ -0,0 +1,190 @@
+package de.awesome_technologies.urcap.dynamixel.dynamixelSDK;
+/*******************************************************************************
+* Copyright 2017 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Author: Ryu Woon Jung (Leon) */
+
+//
+// *********     Read and Write Example      *********
+//
+//
+// Available DXL model on this example : All models using Protocol 1.0
+// This example is tested with a DXL MX-28, and an USB2DYNAMIXEL
+// Be sure that DXL MX properties are already set as %% ID : 1 / Baudnum : 34 (Baudrate : 57600)
+//
+
+import com.ur.urcap.api.domain.data.DataModel;
+
+import java.util.Scanner;
+
+public class ReadWrite {
+
+  short ADDR_MX_TORQUE_ENABLE         = 24;                  // Control table address is different in Dynamixel model
+  short ADDR_MX_GOAL_POSITION         = 30;
+  short ADDR_MX_PRESENT_POSITION      = 36;
+
+  // Protocol version
+  int PROTOCOL_VERSION                = 1;                   // See which protocol version is used in the Dynamixel
+
+  // Default setting
+  byte DXL_ID                         = 1;                   // Dynamixel ID: 1
+  int BAUDRATE                        = 57600;
+  String DEVICENAME                   = "/dev/ttyUSB0";      // Check which port is being used on your controller
+  // ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+  byte TORQUE_ENABLE                  = 1;                   // Value for enabling the torque
+  byte TORQUE_DISABLE                 = 0;                   // Value for disabling the torque
+  short DXL_MINIMUM_POSITION_VALUE    = 100;                 // Dynamixel will rotate between this value
+  short DXL_MAXIMUM_POSITION_VALUE    = 4000;                // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+  int DXL_MOVING_STATUS_THRESHOLD     = 10;                  // Dynamixel moving status threshold
+
+  String KEY_FOR_ESCAPE               = "e";                 // Key for escape
+
+  int COMM_SUCCESS                    = 0;                   // Communication Success result value
+  int COMM_TX_FAIL                    = -1001;               // Communication Tx Failed
+
+
+  boolean opened = false;
+  Scanner scanner;
+  Dynamixel dynamixel;
+
+  int port_num;
+  int index = 0;
+  int dxl_comm_result = COMM_TX_FAIL;
+  short[] dxl_goal_position = new short[]{DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE};
+  byte dxl_error = 0;                                        // Dynamixel error
+  short dxl_present_position = 0;                            // Present position
+
+  public void setVariables(DataModel dataModel){
+    ADDR_MX_TORQUE_ENABLE = (short) dataModel.get("ADDR_MX_TORQUE_ENABLE", 24);
+    ADDR_MX_GOAL_POSITION = (short) dataModel.get("ADDR_MX_GOAL_POSITION", 30);
+    ADDR_MX_PRESENT_POSITION = (short) dataModel.get("ADDR_MX_PRESENT_POSITION", 36);
+    DXL_ID = (byte) dataModel.get("DXL_ID", 1);
+    BAUDRATE = dataModel.get("BAUDRATE", 57600);
+    DEVICENAME = dataModel.get("DEVICENAME", "/dev/ttyUSB0");
+  }
+
+  public void openConnectionAndEnableMovement(){
+    // Instead of getch
+    scanner = new Scanner(System.in);
+
+    // Initialize Dynamixel class for java
+    dynamixel = new Dynamixel();
+
+    // Initialize PortHandler Structs
+    // Set the port path
+    // Get methods and members of PortHandlerLinux or PortHandlerWindows
+    port_num = dynamixel.portHandler(DEVICENAME);
+
+    // Initialize PacketHandler Structs
+    dynamixel.packetHandler();
+
+    // Open port
+    if (dynamixel.openPort(port_num))
+    {
+      System.out.println("Succeeded to open the port!");
+    }
+    else
+    {
+      System.out.println("Failed to open the port!");
+      //System.out.println("Press any key to terminate...");
+      //scanner.nextLine();
+      return;
+    }
+
+    // Set port baudrate
+    if (dynamixel.setBaudRate(port_num, BAUDRATE))
+    {
+      System.out.println("Succeeded to change the baudrate!");
+    }
+    else
+    {
+      System.out.println("Failed to change the baudrate!");
+      //System.out.println("Press any key to terminate...");
+      //scanner.nextLine();
+      return;
+    }
+
+    // Enable Dynamixel Torque
+    dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE);
+    if ((dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)) != COMM_SUCCESS)
+    {
+      System.out.println(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+    }
+    else if ((dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)) != 0)
+    {
+      System.out.println(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error));
+    }
+    else
+    {
+      System.out.println("Dynamixel has been successfully connected");
+    }
+    opened = true;
+  }
+
+  public void moveToPosition(short targetPosition){
+    if (!opened){
+      return;
+    }
+
+      // Write goal position
+      dynamixel.write2ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_GOAL_POSITION, targetPosition);
+      if ((dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)) != COMM_SUCCESS)
+      {
+        System.out.println(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+      }
+      else if ((dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)) != 0)
+      {
+        System.out.println(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error));
+      }
+
+      do
+      {
+        // Read present position
+        dxl_present_position = dynamixel.read2ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_PRESENT_POSITION);
+        if ((dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)) != COMM_SUCCESS)
+        {
+          System.out.println(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+        }
+        else if ((dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)) != 0)
+        {
+          System.out.println(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error));
+        }
+
+        System.out.printf("[ID: %d] GoalPos:%d  PresPos:%d\n", DXL_ID, targetPosition, dxl_present_position);
+
+      } while ((Math.abs(targetPosition - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+  }
+
+  public void disableMovementAndCloseConnection(){
+    if(!opened){
+      return;
+    }
+    // Disable Dynamixel Torque
+    dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+    if ((dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)) != COMM_SUCCESS)
+    {
+      System.out.println(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+    }
+    else if ((dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)) != 0)
+    {
+      System.out.println(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error));
+    }
+
+    // Close port
+    dynamixel.closePort(port_num);
+  }
+}
---
+++ src/test/java/localTest/LinkingTest.java
@@ -0,0 +1,14 @@
+package localTest;
+
+import de.awesome_technologies.urcap.dynamixel.dynamixelSDK.ReadWrite;
+import org.junit.Test;
+
+public class LinkingTest {
+
+    @Test
+    public void testLinking(){
+        ReadWrite readWrite = new ReadWrite();
+        //readWrite.openConnectionAndEnableMovement();
+    }
+
+}
